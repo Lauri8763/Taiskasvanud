7ef56b37128ced8ea7edac8ca88d2ed6c959f7e5
Lauri8763 <120709192+Lauri8763@users.noreply.github.com>
2023-03-09 00:22:19 +0200

    Add project files.

---
+++ Taiskasvanud/Program.cs
@@ -0,0 +1,34 @@
+﻿public static void SumLINQ()
+{
+    Console.BackgroundColor = ConsoleColor.DarkYellow;
+
+    Console.WriteLine("Sum LINQ");
+
+    var sumAge = PeopleList.people.Sum(x => x.Age);
+
+    Console.WriteLine(sumAge);
+
+    Console.WriteLine("Täisealiste isikute koondvanus");
+
+    var sumAdult = 0;
+
+    var numAdults = PeopleList.people.Sum(x =>
+    {
+        if (x.Age >= 18)
+        {
+            //tahan teada t'iskasvanud t;;tajate koondvanust
+            Adult[] people = {new Adult("Moona", 21), new Adult("Ron", 18), new Adult("Anna", 20), new Adult("Jumbo", 21)};
+            sumAdult = PeopleList.people.Sum(x => x.Age);
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    });
+
+    Console.WriteLine("Täiskasvanud isikute arv " + numAdults);
+    Console.WriteLine("Täiskasvanute koondvanuse tulemus " + sumAdult);
+}
+    }
+}
